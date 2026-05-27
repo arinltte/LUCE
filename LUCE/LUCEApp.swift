db5c311dd75194578b98c75b77258402764e1012
arinltte <166289099+arinltte@users.noreply.github.com>
2026-05-27 22:10:06 +0800

    Initial Commit

---
+++ LUCE/LUCEApp.swift
@@ -0,0 +1,17 @@
+//
+//  LUCEApp.swift
+//  LUCE
+//
+//  Created by Chen Jin Shen on 27/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct LUCEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
